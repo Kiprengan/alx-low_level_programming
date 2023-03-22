d832211cc68a66c0bd91076bd24c0faf5e83e998
Denis Kiprono <deniskiprrono881@gmail.com>
2023-03-22 13:00:34 -0700

    Functions to pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,27 @@
 #include "function_pointers.h"
+
 /**
- * int_index - searches for an interger
- * @array: array with data
- * @size: size of the array.
- * @cmp: function pointer
- * Return: count of interger
-*/
+ * int_index - Searches for an integers
+ * @array: Array to search
+ * @size: Size of array
+ * @cmp: Pointer to function used to search integer
+ *
+ * Return: index of integer if cmp function doesn't return 0
+ *         -1 if no element matches the search or if size <= 0
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-        int i;
+	int index;
 
-        if (array == NULL || cmp == NULL)
-                return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	if (size <= 0)
-		return (-1);	
-        for (i = 0; i < size; i++)
-        {
-                if (cmp(array[i]) != 0)
-                        return (i);
-
-        }
-                return (-1);
+		return (-1);
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+	return (-1);
 }
